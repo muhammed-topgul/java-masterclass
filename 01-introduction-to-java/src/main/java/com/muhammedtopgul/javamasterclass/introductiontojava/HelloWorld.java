0b9e3e13da958ff48f17b91daf5a8a0594d6a61c
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-10-20 00:34:30 +0300

    01-introduction-to-java - First Java code. Compile and Run with CLI.

---
+++ 01-introduction-to-java/src/main/java/com/muhammedtopgul/javamasterclass/introductiontojava/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.muhammedtopgul.javamasterclass.introductiontojava;
+
+/**
+ * @author muhammed-topgul created at 19/10/2021 22:59
+ *
+ * <h3>Compile and Run Java Code in CLI</h3>
+ * <b>
+ * 1. compile: 'javac HelloWorld.java' <br>
+ * 2. run: 'java HelloWorld' <br>
+ * 3. see byte code: 'javap -c HelloWorld.class'
+ * </b>
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
