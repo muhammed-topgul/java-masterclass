342e89ee10883b8828d629e6949aac6fabffdd8f
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-10-27 21:48:24 +0300

    02-types-and-variables - variable scope in Java.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/types_and_variables/scope/Scope.java
@@ -0,0 +1,23 @@
+package com.muhammedtopgul.javamasterclass.types_and_variables.scope;
+
+/**
+ * @author muhammed-topgul created at 27/10/2021 21:22
+ * we can define Local Varable name as same as Instance Variable name in Java
+ * if the defined variable call in method, then Java call the 'Local Variable'. This is Shadowing
+ */
+
+public class Scope {
+
+    private String name = "Muhammed in Instance Variable";
+
+    public static void main(String[] args) {
+      Scope scope = new Scope();
+      scope.shadowing();
+    }
+
+    public void shadowing() {
+        String name = "Muhammed in Local Variable";
+        System.out.println(name); // Muhammed in Local Variable
+        System.out.println(this.name); // Muhammed in Instance Variable
+    }
+}
