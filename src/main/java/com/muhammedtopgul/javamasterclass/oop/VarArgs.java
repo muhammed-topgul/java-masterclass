18f11a5d399cf142e5380f0f980eda960c121234
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-11-07 21:25:12 +0300

    07- oop - varargs usage.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/VarArgs.java
@@ -0,0 +1,26 @@
+package com.muhammedtopgul.javamasterclass.oop;
+
+/**
+ * @author muhammed-topgul created at 07/11/2021 20:43
+ */
+
+public class VarArgs {
+
+    public static void main(String[] args) {
+        average(1, 2, 3, 4, 5);
+        average(new int[]{1, 2, 3, 4, 5});
+        int[] array = {5, 9, 13};
+        average(array);
+        average(new int[3]);
+    }
+
+    public static void average(int... numbers) {
+        System.out.println("Type: " + numbers.getClass().getSimpleName());
+        double sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        double average = sum / numbers.length;
+        System.out.println("Average: " + average + "\n");
+    }
+}
