796a7696fff07b32ca1b722b09f5a4486b82a4a1
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-11-18 13:48:49 +0300

    07- oop - override

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/_override/Employee.java
@@ -0,0 +1,66 @@
+package com.muhammedtopgul.javamasterclass.oop._override;
+
+/**
+ * @author muhammed-topgul created at 18/11/2021 13:38
+ */
+
+public class Employee {
+
+    private static final int EMPLOYEE_SALARY = 2000;
+
+    private String id;
+    private String name;
+    private String department;
+    private int year;
+
+    public Employee(String id, String name, String department, int year) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.year = year;
+    }
+
+    public void salary() {
+        System.out.println("Employee Salary: " + year * EMPLOYEE_SALARY);
+    }
+
+    public void work() {
+        System.out.println(name + " is working...");
+    }
+
+    public Employee getPerson() {
+        return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/_override/Manager.java
@@ -0,0 +1,25 @@
+package com.muhammedtopgul.javamasterclass.oop._override;
+
+/**
+ * @author muhammed-topgul created at 18/11/2021 13:39
+ */
+
+public class Manager extends Employee {
+
+    private static final int EMPLOYEE_SALARY = 3500;
+
+    public Manager(String id, String name, String department, int year) {
+        super(id, name, department, year);
+    }
+
+    @Override
+    public void salary() {
+        System.out.println("Manager Salary: " + getYear() * EMPLOYEE_SALARY);
+    }
+
+    // Covariant return type
+    @Override
+    public Manager getPerson() {
+        return this;
+    }
+}
