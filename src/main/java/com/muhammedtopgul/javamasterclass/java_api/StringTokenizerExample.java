97e5138902334c9303f297a14ee23009fa602662
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-11-21 22:46:52 +0300

    08- java api.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/java_api/StringTokenizerExample.java
@@ -0,0 +1,18 @@
+package com.muhammedtopgul.javamasterclass.java_api;
+
+import java.util.StringTokenizer;
+
+/**
+ * @author muhammed-topgul created at 21/11/2021 19:40
+ */
+
+public class StringTokenizerExample {
+
+    public static void main(String[] args) {
+        String text = "Java is the best in the world.";
+
+        StringTokenizer stringTokenizer = new StringTokenizer(text, " ", false);
+        while (stringTokenizer.hasMoreElements())
+            System.out.println(stringTokenizer.nextToken());
+    }
+}
