fa55d4fedb151d0c40c975780316e81d7da7ef14
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-11-24 12:16:53 +0300

    007- oop - upcasting.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/advanced/polymorphism/upcasting/Employee.java
@@ -0,0 +1,67 @@
+package com.muhammedtopgul.javamasterclass.oop.advanced.polymorphism.upcasting;
+
+/**
+ * @author muhammed-topgul created at 24/11/2021 11:24
+ */
+
+public class Employee {
+
+    public static final int BASE_SALARY = 2000;
+
+    private int no;
+    private String name;
+    private int year;
+    private String department;
+
+    public Employee(int no, String name, int year, String department) {
+        this.no = no;
+        this.name = name;
+        this.year = year;
+        this.department = department;
+    }
+
+    public void work() {
+        System.out.println("Employee is working...");
+    }
+
+    public double calculateSalary() {
+        return year * BASE_SALARY;
+    }
+
+    public void printInfo() {
+        System.out.println("Employee, no: " + no + ", name: " + name + ", year: " + year + ", department:  " + department + "\n");
+    }
+
+    //
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/advanced/polymorphism/upcasting/Main.java
@@ -0,0 +1,57 @@
+package com.muhammedtopgul.javamasterclass.oop.advanced.polymorphism.upcasting;
+
+/**
+ * @author muhammed-topgul created at 24/11/2021 11:39
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee(0, "Melissa", 4, "Human Resource");
+        employee.printInfo();
+
+        Manager manager = new Manager(1, "Muhammed", 2, "Software", "Java Teams");
+        Employee employee1 = manager;
+
+        System.out.println(employee1.calculateSalary());
+        employee1.printInfo();
+
+        // directly
+        Employee employee2 = new Manager(2, "Jack", 4, "Software", "Java Teams");
+        employee2.printInfo();
+
+        Manager manager1 = new Director(3, "John", 6, "Accounting", "Finance");
+        manager1.printInfo();
+
+        Employee employee3 = new Director(4, "Linda", 1, "Accounting", "Finance");
+        employee3.printInfo();
+
+
+        // payment
+        PayrollOffice payrollOffice = new PayrollOffice();
+        payrollOffice.paySalary(employee);
+        payrollOffice.paySalary(employee1);
+        payrollOffice.paySalary(employee2);
+        payrollOffice.paySalary(employee3);
+        payrollOffice.paySalary(manager);
+        payrollOffice.paySalary(manager1);
+    }
+
+    private static class PayrollOffice {
+
+        public void paySalary(Employee employee) {
+            if (employee == null) {
+                throw new NullPointerException("Employee must not be null.");
+            }
+            if (employee instanceof Director) {
+                System.out.println("Paying for a director...");
+            } else if (employee instanceof Manager) {
+                System.out.println("Paying for a manager...");
+            } else if (employee instanceof Employee) {
+                System.out.println("Paying for an employee...");
+            }
+            double salary = employee.calculateSalary();
+            System.out.println("Paying a salary of " + salary + " to " + employee.getName() + " \n");
+        }
+    }
+}
