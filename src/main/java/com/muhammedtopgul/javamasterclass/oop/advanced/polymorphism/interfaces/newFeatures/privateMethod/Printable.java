671e7852be5bd02a321b8b295e72776b539f89fd
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-11-30 09:52:57 +0300

    007- oop - interfaces new features - private method.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/advanced/polymorphism/interfaces/newFeatures/privateMethod/Printable.java
@@ -0,0 +1,30 @@
+package com.muhammedtopgul.javamasterclass.oop.advanced.polymorphism.interfaces.newFeatures.privateMethod;
+
+/**
+ * @author muhammed-topgul created at 30/11/2021 00:05
+ */
+
+public interface Printable {
+
+    void view();
+
+    void print();
+
+    default void format() {
+        staticInfo("***static private*** format() must be implement.");
+        info("***private*** format() must be implement.");
+        throw new UnsupportedOperationException("Format function did not override.");
+    }
+
+    static void startPrinting() {
+        staticInfo("***static private*** Printing is starting...");
+    }
+
+    private static void staticInfo(String info) {
+        System.out.println(info);
+    }
+
+    private void info(String info) {
+        System.out.println(info);
+    }
+}
---
+++ src/main/java/com/muhammedtopgul/javamasterclass/oop/advanced/polymorphism/interfaces/newFeatures/privateMethod/Word.java
@@ -0,0 +1,24 @@
+package com.muhammedtopgul.javamasterclass.oop.advanced.polymorphism.interfaces.newFeatures.privateMethod;
+
+/**
+ * @author muhammed-topgul created at 30/11/2021 00:08
+ */
+
+public class Word implements Printable {
+
+    @Override
+    public void view() {
+        System.out.println("Word file viewed.");
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Word file printed.");
+    }
+
+    @Override
+    public void format() {
+        // Printable.super.format();
+        System.out.println("Word file formatted.");
+    }
+}
