51783d4f4227a98b9a9b907a1481805dcb470cbd
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-12-02 16:40:24 +0300

    009- exceptions - try-catch-finally.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/exceptions/TryCatchFinally.java
@@ -0,0 +1,37 @@
+package com.muhammedtopgul.javamasterclass.exceptions;
+
+import java.io.IOException;
+
+/**
+ * @author muhammed-topgul created at 02/12/2021 15:55
+ */
+
+public class TryCatchFinally {
+
+    public static void main(String[] args) {
+        TryCatchFinally tryCatchFinally = new TryCatchFinally();
+
+        int var1 = 10;
+        int var2 = 1;
+        try {
+            System.out.println(tryCatchFinally.divide(var1, var2));
+            System.out.println("After divide()");
+        } catch (ArithmeticException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("In finally...");
+        }
+    }
+
+    public double divide(int arg1, int arg2) throws ArithmeticException, IOException {
+        if (arg2 == 0) {
+            throw new ArithmeticException("Cannot divide to 0");
+        }
+
+        if (Math.random() < 0.5) {
+            throw new IOException();
+        }
+
+        return (double) arg1 / arg2;
+    }
+}
