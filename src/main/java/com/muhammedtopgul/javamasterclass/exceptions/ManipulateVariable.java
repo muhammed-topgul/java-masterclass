6d6bc53a44b0d7a59d7cda90a20ff627d9ebe168
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-12-02 17:33:16 +0300

    009- exceptions - try with resources.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/exceptions/ManipulateVariable.java
@@ -0,0 +1,44 @@
+package com.muhammedtopgul.javamasterclass.exceptions;
+
+/**
+ * @author muhammed-topgul created at 02/12/2021 17:17
+ */
+
+public class ManipulateVariable {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.value = "A";
+        manipulate(user);
+
+        System.out.println(user.value);
+    }
+
+    public static void manipulate(User user) {
+        try {
+            System.out.println("user.value = B");
+            user.value = "B";
+            throwException(user);
+            System.out.println("user.value = X");
+            user.value = "X";
+        } catch (Exception e) {
+            System.out.println("user.value = D");
+            user.value = "D";
+        } finally {
+            System.out.println("user.value = E");
+            user.value = "E";
+        }
+    }
+
+    public static void throwException(User user) throws Exception {
+        System.out.println("user.value = C");
+        user.value = "C";
+        System.out.println("Throwing an exception!");
+        throw new Exception();
+    }
+}
+
+class User {
+
+    public String value;
+}
---
+++ src/main/java/com/muhammedtopgul/javamasterclass/exceptions/tryWithResources/Main.java
@@ -0,0 +1,81 @@
+package com.muhammedtopgul.javamasterclass.exceptions.tryWithResources;
+
+/**
+ * @author muhammed-topgul created at 02/12/2021 17:15
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+//        run0();
+        run1();
+    }
+
+    public static void run0() {
+        ClosableClass closableClass = null;
+        try {
+            closableClass = new ClosableClass("unique", false);
+            System.out.println("in try block.");
+            throwException();
+            closableClass.close();
+        } catch (Exception e) {
+            System.out.println("in catch block.");
+            try {
+                if (closableClass.isOpen())
+                    closableClass.close();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        } finally {
+            System.out.println("in finally()");
+            try {
+                if (closableClass.isOpen())
+                    closableClass.close();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+        System.out.println("After try block.");
+    }
+
+    public static void run1() {
+        try (ClosableClass closableClass = new ClosableClass("unique", false)) {
+            System.out.println("in try block.");
+            throwException();
+        } catch (Exception e) {
+            System.out.println("in catch block.");
+        } finally {
+            System.out.println("finally here :)");
+        }
+        System.out.println("After try block.");
+    }
+
+    public static void run2() {
+        try (ClosableClass closableClass1 = new ClosableClass("first", false); ClosableClass closableClass2 = new ClosableClass("second", false)) {
+            System.out.println("in try block.");
+            throwException();
+        } catch (Exception e) {
+            System.out.println("in catch block.");
+        } finally {
+            System.out.println("finally here :)");
+        }
+        System.out.println("After try block.");
+    }
+
+    public static void run3() {
+        try (ClosableClass closableClass1 = new ClosableClass("first", false); ClosableClass closableClass2 = new ClosableClass("second", true)) {
+            System.out.println("in try block.");
+            throwException();
+        } catch (Exception e) {
+            System.out.println("in catch block.");
+        } finally {
+            System.out.println("finally here :)");
+        }
+        System.out.println("After try block.");
+    }
+
+    public static void throwException() throws Exception {
+        System.out.println("Throwing an exception!");
+        throw new Exception();
+    }
+}
