d40d3082714b65a6b2c5fa92b43da75b20babc94
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-12-08 22:04:46 +0300

    010- fp - anonymous classes.

---
+++ src/main/java/com/muhammedtopgul/javamasterclass/functionalProgramming/anonymousClasses/TimerExample1.java
@@ -0,0 +1,71 @@
+package com.muhammedtopgul.javamasterclass.functionalProgramming.anonymousClasses;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+/**
+ * @author muhammed-topgul
+ * @created at 08.12.2021 21:38
+ */
+
+public class TimerExample1 {
+
+    public static void main(String[] args) {
+        ActionListener actionListener = new TimePrinter1();
+        Timer timer1 = new Timer(1000, actionListener);
+        // timer1.start();
+
+        Timer timer2  = new Timer(1000, new HelloPrinter());
+        // timer2.start();
+
+        actionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Timer3");
+                Toolkit.getDefaultToolkit().beep();
+            }
+        };
+
+        Timer timer3 = new Timer(1000, actionListener);
+        timer3.start();
+
+        Timer timer4 = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Timer4");
+                Toolkit.getDefaultToolkit().beep();
+            }
+        });
+        timer4.start();
+
+        Timer timer5 = new Timer(1000, (e) -> {
+            System.out.println("Timer5");
+            Toolkit.getDefaultToolkit().beep();
+        });
+        timer5.start();
+
+        JOptionPane.showMessageDialog(null, "Quit Program?");
+        System.exit(0);
+    }
+}
+
+class TimePrinter1 implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Time is " + new Date());
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
+
+class HelloPrinter implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Hello :)");
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
